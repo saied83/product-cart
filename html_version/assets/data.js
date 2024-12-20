78ce6b56b711f4c9b5ae5aa4c09a1d7515a64277
Md Saiedur Rahman Saied <saied@pop-os.lan>
2024-12-20 21:41:37 +0600

    finish designing

---
+++ html_version/assets/data.js
@@ -0,0 +1,23 @@
+export const productData = {
+  productName: "Classy Modern Smart watch",
+  totalReviews: 2,
+  avgRating: 3.5,
+  price: 99.0,
+  latestPrice: 79.0,
+  description:
+    "I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system.",
+  type: "Watch",
+  modelNumber: "Forerunner 290XT",
+  bandColorWithImage: {
+    purple: "./assets/images/purple_band.png",
+    blue: "./assets/images/blue_band.png",
+    cyan: "./assets/images/cyan_band.png",
+    black: "./assets/images/black_band.png",
+  },
+  priceWithSize: {
+    s: 69,
+    m: 79,
+    l: 89,
+    xl: 99,
+  },
+};
